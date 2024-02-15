8ba000ff15a5cd3fb0ef20840c2dfac7391e0ad9
Flowseal <alegiumselly@gmail.com>
2024-02-15 03:16:58 +0300

    Выполнен вариант Replace

--- Replace/Replace.cpp
+++ Replace/Replace.cpp
@@ -5,22 +5,37 @@
 std::string ReplaceString(const std::string& subject,
 	const std::string& searchString, const std::string& replacementString)
 {
-	// Эта функция написана не до конца. Напишите недостающий код самостоятельно
-
+	const size_t searchLength = searchString.length();
 	size_t pos = 0;
-	// Результат будет записан в новую строку result, оставляя строку subject неизменной
-	// Какие преимущества есть у этого способа по сравнению с алгоритмом, выполняющим
-	// замену прямо в строке subject?
-	std::string result;
-	while (pos < subject.length())
-	{
-		// Находим позицию искомой строки, начиная с pos
-		size_t foundPos = subject.find(searchString, pos);
-		// В результирующую строку записываем текст из диапазона [pos,foundPos)
-		result.append(subject, pos, foundPos - pos);
 
-		// Напишите недостающий код самостоятельно, чтобы функция работала корректно
+	// Результат будет записан в новую строку result, оставляя строку subject неизменной
+	std::string result;
+
+	if (searchString.length() > 0)
+	{
+		while (pos < subject.length())
+		{
+			// Находим позицию искомой строки, начиная с pos
+			size_t foundPos = subject.find(searchString, pos);
+
+			// Если не нашли текст для замены - выходим из цикла и записываем остатки subject
+			if (foundPos == std::string::npos)
+				break;
+
+			// В результирующую строку записываем текст из диапазона [pos,foundPos)
+			result.append(subject, pos, foundPos - pos);
+
+			// В результирующую строку записываем текст для замены
+			result.append(replacementString);
+
+			// Пропускаем найденную подстроку
+			pos = foundPos + searchLength;
+		}
 	}
+
+	// Записываем остатки subject в результирующую строку
+	result.append(subject, pos, subject.length());
+
 	return result;
 }
 
@@ -35,6 +50,39 @@ void CopyStreamWithReplacement(std::istream& input, std::ostream& output,
 	}
 }
 
+void CopyFileWithReplacement(const char* inputFileName, const char* outputFileName,
+	const char* searchString, const char* replacementString)
+{
+	std::ifstream inputFile;
+	inputFile.open(inputFileName);
+	if (!inputFile.is_open())
+	{
+		throw std::runtime_error("Error opening input file!");
+	}
+
+	std::ofstream outputFile;
+	outputFile.open(outputFileName);
+	if (!outputFile.is_open())
+	{
+		throw std::runtime_error("Error opening output file!");
+	}
+
+	std::string search = searchString;
+	std::string replace = replacementString;
+
+	CopyStreamWithReplacement(inputFile, outputFile, search, replace);
+
+	if (inputFile.bad())
+	{
+		throw std::runtime_error("Error reading input file!");
+	}
+
+	if (!outputFile.flush())
+	{
+		throw std::runtime_error("Error writing to output file!");
+	}
+}
+
 int main(int argc, char* argv[])
 {
 	if (argc != 5)
@@ -44,22 +92,15 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	// Самостоятельно выделите код копирования содержимого файла в отдельную функцию CopyFileWithReplacement,
-	// принимающую имена файлов, а также строки для поиска и замены
-	// Добавьте обработку ошибок
-
-	std::ifstream inputFile;
-	inputFile.open(argv[1]);
-
-	std::ofstream outputFile;
-	outputFile.open(argv[2]);
-
-	std::string search = argv[3];
-	std::string replace = argv[4];
-
-	CopyStreamWithReplacement(inputFile, outputFile, search, replace);
-	// Подумайте, для чего здесь вызывается flush?
-	outputFile.flush();
+	try
+	{
+		CopyFileWithReplacement(argv[1], argv[2], argv[3], argv[4]);
+	}
+	catch (const std::exception& exception)
+	{
+		std::cout << exception.what() << std::endl;
+		return 1;
+	}
 
 	return 0;
 }
--- Replace/test.bat
+++ Replace/test.bat
@@ -6,21 +6,43 @@ set PROGRAM="%~1"
 
 rem При запуске без параметров ожидается ненулевой код возврата
 %PROGRAM% > nul
-if NOT ERRORLEVEL 1 goto err
+if %ERRORLEVEL% EQU 0 goto err
+echo Test1 passed
+
+rem При запуске с несуществующим входным файлом ожидается ненулевой код возврата
+%PROGRAM% test-data\not-existing.txt "%TEMP%\fox.txt" bird cat > nul
+if %ERRORLEVEL% EQU 0 goto err
+echo Test2 passed
 
 rem При запуске с правильными параметрами ожидается нулевой код возврата
 %PROGRAM% test-data\fox.txt "%TEMP%\fox.txt" bird cat
-if ERRORLEVEL 1 goto err
+if %ERRORLEVEL% NEQ 0 goto err
 fc.exe "%TEMP%\fox.txt" test-data\fox.txt >nul
-if ERRORLEVEL 1 goto err
+if %ERRORLEVEL% NEQ 0 goto err
+echo Test3 passed
 
 rem При запуске с правильными параметрами ожидается нулевой код возврата
 %PROGRAM% test-data\fox.txt "%TEMP%\fox.txt" dog cat
-if ERRORLEVEL 1 goto err
+if %ERRORLEVEL% NEQ 0 goto err
 fc.exe "%TEMP%\fox.txt" test-data\fox-replace-dog-with-cat.txt >nul
-if ERRORLEVEL 1 goto err
+if %ERRORLEVEL% NEQ 0 goto err
+echo Test4 passed
 
-echo OK
+rem При запуске с правильными параметрами ожидается нулевой код возврата
+%PROGRAM% test-data\fox.txt "%TEMP%\fox.txt" ""  cat
+if %ERRORLEVEL% NEQ 0 goto err
+fc.exe "%TEMP%\fox.txt" test-data\fox.txt >nul
+if %ERRORLEVEL% NEQ 0 goto err
+echo Test5 passed
+
+rem При запуске с правильными параметрами ожидается нулевой код возврата
+%PROGRAM% test-data\digits.txt "%TEMP%\digits.txt" 1231234 0
+if %ERRORLEVEL% NEQ 0 goto err
+fc.exe "%TEMP%\digits.txt" test-data\digits-replace-1231234-with-0.txt >nul
+if %ERRORLEVEL% NEQ 0 goto err
+echo Test6 passed
+
+echo All tests passed!
 exit 0
 
 :err
